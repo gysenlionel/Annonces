12c5a3e4bd3850b3ae1fa30dd94f4838be8f10a4
Gysen Lionel <gysenlionel@gmail.com>
2022-07-27 18:56:34 +0200

    mode maintenance

---
+++ src/EventListener/MaintenanceListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Twig\Environment;
+
+class MaintenanceListener
+{
+    private $maintenance;
+    private $twig;
+
+    public function __construct($maintenance, Environment $twig)
+    {
+        $this->maintenance = $maintenance;
+        $this->twig = $twig;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        // On vérifie si le fichier .maintenance n'existe pas
+        if (!file_exists($this->maintenance)) {
+            return;
+        }
+
+        $event->setResponse(
+            new Response(
+                $this->twig->render("maintenance/maintenance.html.twig"),
+                Response::HTTP_SERVICE_UNAVAILABLE
+            )
+        );
+        $event->stopPropagation();
+    }
+}
